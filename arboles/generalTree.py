7b8948988d59964d2fb9bb0c0102320cbb4439be
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2025-04-09 09:59:14 -0500

    Arboles

---
+++ arboles/generalTree.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.value: int = value
+        self.children: list[Node] = []
+
+    def __repr__(self):
+        return f"{self.value} -> {self.children}"
+    
+class GeneralTree:
+    def __init__(self):
+        self.root: Node = None
+    
+    def insert(self, parent, child, flag = True, current = None):
+        if flag == True:
+            current = self.root
+            
+        if self.root == None:
+            self.root = Node(parent)
+            node_child = Node(child)
+            self.root.children.append(node_child)
+            return 
+        
+        if current.value == parent:
+            node_child = Node(child)
+            current.children.append(node_child)
+            return
+        
+        else:
+            for ch in current.children:
+                self.insert(parent,child, flag=False, current=ch)
+    
+    def __repr__(self):
+        return f"{self.root}"
+
+arbol = GeneralTree()
+arbol.insert(10, 20)
+arbol.insert(10, 7)
+arbol.insert(7, 8)
+print(arbol)
+
