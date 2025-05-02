168a0ef1434dd28063fd59660feb9d37a35ee484
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2025-05-02 15:54:49 -0500

    grafos

---
+++ Grafos/lista_de_adyasencia.py
@@ -0,0 +1,39 @@
+class Graph:
+    def __init__(self):
+        self.adj_list: dict[int, list[int]] = {}
+        self.whigth = 0
+        self.size = 0
+        self.deafault_whigth = 1
+    
+    def add_vertex(self, vertex):
+        if vertex in self.adj_list:
+            return
+        
+        self.adj_list[vertex] = []
+    
+    def add_edge(self, vertex1, vertex2, direct=False, wigth= None):
+        if vertex1 not in self.adj_list:
+            self.add_vertex(vertex1)
+        
+        if vertex2 not in self.adj_list:
+            self.add_vertex(vertex2)
+        
+        w = vertex1 if wigth is None else (vertex1, wigth)
+        if direct:
+            if vertex1 not in self.adj_list[vertex2]:
+                self.adj_list[vertex2].append(w)
+
+        w = vertex2 if wigth is None else (vertex2, wigth)
+        self.adj_list[vertex1].append(w)
+
+    def __repr__(self):
+        return str(self.adj_list)
+
+g = Graph()
+g.add_vertex(1)
+g.add_vertex(2)
+g.add_vertex(4)
+g.add_vertex(6)
+g.add_vertex(9)
+g.add_edge(1, 4, True,wigth=10)
+print(g)
---
+++ Grafos/matriz_de_adyasencia.py
@@ -0,0 +1,51 @@
+class Graph:
+    def __init__(self):
+        self.size = 0
+        self.Nodes: list[int] = []
+        self.type: str = None
+        self.adj_matrix: list[list[int]] = []
+        self.whigth = 0
+        self.deafault_whigth = 1
+    
+    def add_vertex(self, vertex):
+        if vertex in self.Nodes:
+            return
+        self.Nodes.append(vertex)
+
+        for row in self.adj_matrix:
+            row.append(0)
+        
+        self.adj_matrix.append([0] * (self.size+1))
+        self.size += 1
+    
+    def add_edge(self, vertex1, vertex2, direct=False, wigth= None):
+        if vertex1 not in self.Nodes:
+            self.add_vertex(vertex1)
+        
+        if vertex2 not in self.Nodes:
+            self.add_vertex(vertex2)
+        
+        pos_vertex1 = self.Nodes.index(vertex1)
+        pos_vertex2 = self.Nodes.index(vertex2)
+        
+        w = self.deafault_whigth if wigth is None else wigth
+        if direct:
+            self.adj_matrix[pos_vertex2][pos_vertex1] = w
+            
+        self.adj_matrix[pos_vertex1][pos_vertex2] = w
+
+    def __repr__(self):
+        rep_str = ""
+        for i in self.adj_matrix:
+            rep_str += str(i) + "\n"
+        
+        return rep_str
+
+g = Graph()
+g.add_vertex(1)
+g.add_vertex(2)
+g.add_vertex(4)
+g.add_vertex(6)
+g.add_vertex(9)
+g.add_edge(1,4)
+print(g)
