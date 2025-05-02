8d51acad6024b9a26f93aa33e2ad454e1cc6484c
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2025-05-02 16:03:19 -0500

    prueba add_edge

--- Grafos/lista_de_adyasencia.py
+++ Grafos/lista_de_adyasencia.py
@@ -3,7 +3,6 @@ class Graph:
         self.adj_list: dict[int, list[int]] = {}
         self.whigth = 0
         self.size = 0
-        self.deafault_whigth = 1
     
     def add_vertex(self, vertex):
         if vertex in self.adj_list:
@@ -22,9 +21,10 @@ class Graph:
         if direct:
             if vertex1 not in self.adj_list[vertex2]:
                 self.adj_list[vertex2].append(w)
-        
+
         w = vertex2 if wigth is None else (vertex2, wigth)
-        self.adj_list[vertex1].append(w)
+        if vertex2 not in self.adj_list[vertex1]:
+            self.adj_list[vertex1].append(w)
 
     def __repr__(self):
         return str(self.adj_list)
@@ -35,5 +35,10 @@ g.add_vertex(2)
 g.add_vertex(4)
 g.add_vertex(6)
 g.add_vertex(9)
-g.add_edge(1, 4, True,wigth=10)
+g.add_edge(1, 4)
+g.add_edge(1, 5)
+g.add_edge(3, 5)
+g.add_edge(3, 9)
+g.add_edge(3, 4)
+
 print(g)
